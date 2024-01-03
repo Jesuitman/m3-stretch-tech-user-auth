b918afd78ff343a06c8ed447aabaca119ad8a50c
Alexandre Hastings <alex.hastings.1227@gmail.com>
2024-01-03 17:54:10 -0500

    testing 0auth

--- src/App/App.js
+++ src/App/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import WikipediaSearch from '../Search/Search.js';
+import LoginButton from '../Main/Main';
 import './App.css';
+import { useAuth0 } from '@auth0/auth0-react';
 
 function App() {
+  
+  const { isAuthenticated } = useAuth0();
+
+
   return (
     <div className="App">
       <header className="App-header">
         <h1 className='header-text'>H8rAid!</h1>
-        <button className='login-button'>Login</button>
+        {isAuthenticated ? (
+          <button className='login-button'>Logout</button>
+        ) : (
+          <LoginButton />
+        )}
       </header>
       <Routes>
         <Route path='/' element={<WikipediaSearch />} />
