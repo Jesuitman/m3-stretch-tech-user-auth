3520fcda471e298b3ecb7cdfd7b763c182f91e33
Joseph Stewart <JosephStewart1752@gmail.com>
2024-01-06 13:49:22 -0500

    Add conditional error handling

--- src/Errors/ErrorBoundary.js
+++ src/Errors/ErrorBoundary.js
@@ -1,20 +1,26 @@
-import React, { Component } from 'react';
 import ErrorPage from '../Errors/Errors.js';
+import React, { Component } from 'react';
 
 class ErrorBoundary extends Component {
-  constructor(props) {
-    super(props);
+  constructor(error) {
+    super(error);
     this.state = {
       hasError: false,
       errorMessage: '',
     };
   }
-
+ 
   static getDerivedStateFromError(error) {
+    if (error.message === 'Network response was not ok') {
+        return { hasError: true, errorMessage: 'Error in SpecificComponent' };
+    } 
+    if(error.message === 'Error fetching Wikipedia page:') {
+        return{ hasError: true, errorMessage: 'Cannot fetch Wikipedia page'}
+    }
     return { hasError: true, errorMessage: error.toString() };
   }
 
-  componentDidCatch(error, errorInfo) {
+  componentsDidCatch(error, errorInfo) {
     console.error('ErrorBoundary caught an error:', error, errorInfo);
   }
 
