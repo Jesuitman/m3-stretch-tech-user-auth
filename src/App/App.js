2087c860af886117aa7520dbabec39f82826f86e
Joseph Stewart <JosephStewart1752@gmail.com>
2024-01-06 13:49:45 -0500

    Add ErrorBoundary

--- src/App/App.js
+++ src/App/App.js
@@ -4,12 +4,13 @@ import WikipediaSearch from '../Search/Search.js';
 import './App.css';
 import Card from '../Card/Card.js';
 import useSearchResults from '../hooks/useSearchResults.js';
+import ErrorBoundary from '../Errors/ErrorBoundary.js'
 
 
 function App() {
   const randomSearchInputs = ['4chan', 'Titan (Submersible)', 'Billy Mitchell (gamer)', 'You Showed Me', 'Ezra Miller', 'Russel Brand', 'Bernie Madoff', 'Amy Winehouse']
   const { triggerSearch, controversies, initialResults } = useSearchResults();
-
+  
   const randomSearch = () => {
     const randomIndex = Math.floor(Math.random() * randomSearchInputs.length);
     triggerSearch(randomSearchInputs[randomIndex]);
@@ -21,28 +22,30 @@ function App() {
   // }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1 className='header-text'>H8rAid!</h1>
-        <button className='login-button'>Login</button>
-      </header>
-      <Routes>
-        <Route path='/' element={<WikipediaSearch />} />
-        {/* <Route path='/main' element={<Navigate to='/' />} /> */}
-        <Route path="article/:id" element={<WikipediaSearch />} />
-      </Routes>
-      <footer className='footer-card'>
-        <h2 className='footer-text'>Random Controversy</h2>
-        {controversies[0] && <h2 id='resultName'>{initialResults.title}</h2>}
-        {controversies.map((item, i) => (
-          <Card
-            key={i}
-            title={item.parse.title}
-            snippet={item.parse.text["*"]}
-          />
-        ))}
-      </footer>
-    </div>
+    <ErrorBoundary>
+      <div className="App">
+        <header className="App-header">
+          <h1 className='header-text'>H8rAid!</h1>
+          <button className='login-button'>Login</button>
+        </header>
+        <Routes>
+          <Route path='/' element={<WikipediaSearch />} />
+          {/* <Route path='/main' element={<Navigate to='/' />} /> */}
+          <Route path="article/:id" element={<WikipediaSearch />} />
+        </Routes>
+        <footer className='footer-card'>
+          <h2 className='footer-text'>Random Controversy</h2>
+          {controversies[0] && <h2 id='resultName'>{initialResults.title}</h2>}
+          {controversies.map((item, i) => (
+            <Card
+              key={i}
+              title={item.parse.title}
+              snippet={item.parse.text["*"]}
+            />
+          ))}
+        </footer>
+      </div>
+    </ErrorBoundary>
   );
 }
 
